b8f3faedbc6b9d98232b72c62a1b0ca5888f04bd
Joseph Harris <joeharris461767@gmail.com>
2022-07-14 21:02:42 +0100

    Add database configuration

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -1,6 +1,21 @@
+// https://docs.nestjs.com/techniques/configuration
 export default () => ({
     port: parseInt(process.env.PORT, 10) || 3000,
     database: {
-        
+        host: process.env.DATABASE_HOST || 'localhost',
+        port: parseInt(process.env.DATABASE_PORT, 10) || 5432,
+        username: process.env.DATABASE_USER || 'postgres',
+        password: process.env.DATABASE_PASS || 'root',
+        name: process.env.DATABASE_NAME || 'ageuk',
+    },
+    jwt: {
+        accessToken: {
+            secret: process.env.JWT_ACCESS_SECRET || 'access_secret',
+            expiresIn: process.env.JWT_ACCESS_EXPIRY || '60s'
+        },
+        refreshToken: {
+            secret: process.env.JWT_REFRESH_SECRET || 'refresh_secret',
+            expiresIn: process.env.JWT_REFRESH_EXPIRY || '5m'
+        }
     }
 });
