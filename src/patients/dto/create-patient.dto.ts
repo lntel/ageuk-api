3843b15007a47dc0e8648c295334183d6ab5d8e7
Joseph Harris <joeharris461767@gmail.com>
2022-08-22 18:46:39 +0100

    Add custom messages to DTO Changed column names

--- src/patients/dto/create-patient.dto.ts
+++ src/patients/dto/create-patient.dto.ts
@@ -1,4 +1,4 @@
-import { IsString, Matches, IsOptional, IsDateString, IsDefined, IsNotEmptyObject, ValidateNested, IsArray, IsInt, IsNotEmpty, IsMobilePhone, IsBoolean } from 'class-validator';
+import { IsString, Matches, IsOptional, IsDateString, IsDefined, IsNotEmptyObject, ValidateNested, IsArray, IsInt, IsNotEmpty, IsMobilePhone, IsBoolean, isDefined, IsEmpty } from 'class-validator';
 import { Type } from 'class-transformer';
 import { Assessment } from '../entities/assessment.entity';
 
@@ -6,11 +6,11 @@ export class CreatePatientAssessmentDto {
   
   @IsDefined()
   @IsBoolean()
-  hasDnacpr: boolean;
+  dnacpr: boolean;
   
   @IsDefined()
   @IsBoolean()
-  riskOfPressureSores: boolean;
+  riskOfPressure: boolean;
   
   @IsDefined()
   @IsBoolean()
@@ -18,31 +18,27 @@ export class CreatePatientAssessmentDto {
   
   @IsDefined()
   @IsBoolean()
-  medicationAssistant: boolean;
+  marChart: boolean;
   
   @IsDefined()
   @IsBoolean()
-  marChartInPlace: boolean;
+  careAssistant: boolean;
   
   @IsDefined()
   @IsBoolean()
-  personalCareAssistant: boolean;
+  pressureSore: boolean;
   
   @IsDefined()
   @IsBoolean()
-  pressureSores: boolean;
+  weightBear: boolean;
   
   @IsDefined()
   @IsBoolean()
-  weightBearing: boolean;
+  painSymptom: boolean;
   
   @IsDefined()
   @IsBoolean()
-  painAndSymptomSupport: boolean;
-  
-  @IsDefined()
-  @IsBoolean()
-  medicationPresent: boolean;
+  medication: boolean;
 
   @IsDefined()
   @IsBoolean()
@@ -50,7 +46,7 @@ export class CreatePatientAssessmentDto {
 
   @IsOptional()
   @IsDefined()
-  syringeDriverInstallationDate?: Date;
+  syringeDriverSetupDate?: Date;
 }
 export class CreatePatientDto {
   @IsDefined({
@@ -61,40 +57,58 @@ export class CreatePatientDto {
   })
   id: string;
 
-  @IsDefined()
+  @IsDefined({
+    message: 'You must enter a first name'
+  })
   @IsString()
   firstName: string;
 
   @IsOptional()
   @IsDefined()
-  @IsString()
+  @IsString({
+    message: 'You have entered an invalid middle name(s)'
+  })
   middleNames: string;
 
-  @IsDefined()
+  @IsDefined({
+    message: 'You must enter a surname'
+  })
   @IsString()
   surname: string;
 
-  @IsNotEmpty()
+  @IsDefined({
+    message: 'You must enter a phone number'
+  })
   @Matches(
     new RegExp(
       /^(((\+44\s?\d{4}|\(?0\d{4}\)?)\s?\d{3}\s?\d{3})|((\+44\s?\d{3}|\(?0\d{3}\)?)\s?\d{3}\s?\d{4})|((\+44\s?\d{2}|\(?0\d{2}\)?)\s?\d{4}\s?\d{4}))(\s?\#(\d{4}|\d{3}))?$/,
     ),
+    {
+      message: 'You have entered an invalid phone number'
+    }
   )
   telephoneNumber: string;
 
-  @IsDefined()
+  @IsDefined({
+    message: 'You must provide the patients address line'
+  })
   @IsString()
   addressLine: string;
 
-  @IsNotEmpty()
+  @IsDefined({
+    message: 'You must enter a DOB for the patient'
+  })
   @IsDateString()
   dob: Date;
 
+  // ? Will this be taken out? Carers say that GP's do not like one individual to be tied to it
   @IsOptional()
   @IsString()
   gpFullname: string;
 
-  @IsDefined()
+  @IsDefined({
+    message: 'You must enter a town or city'
+  })
   @IsString()
   city: string;
 
@@ -111,7 +125,9 @@ export class CreatePatientDto {
   @IsArray()
   allergies: string[];
 
-  @IsDefined()
+  @IsDefined({
+    message: 'You must enter a postcode'
+  })
   @Matches(
     new RegExp(
       /^([A-Za-z][A-Ha-hJ-Yj-y]?[0-9][A-Za-z0-9]? ?[0-9][A-Za-z]{2}|[Gg][Ii][Rr] ?0[Aa]{2})$/,
@@ -123,31 +139,33 @@ export class CreatePatientDto {
   postcode: string;
 
   // This is usually only weeks or days
-  @IsDefined()
+  @IsDefined({
+    message: 'You must select a prognosis'
+  })
   @IsString()
   prognosis: string;
 
-  @IsDefined()
+  @IsDefined({
+    message: 'You must enter a date that the care provision will start'
+  })
   @IsDateString()
   startDate: Date;
 
-  @IsNotEmpty()
-  @IsDateString()
-  sixWeekReview: Date;
-
-  @IsNotEmpty()
-  @IsDateString()
-  eightWeekReview: Date;
-
-  @IsDefined()
+  @IsDefined({
+    message: 'You must select a GP'
+  })
   @IsInt()
   gpId: number;
 
-  @IsDefined()
+  @IsDefined({
+    message: 'You must enter where the referral came from'
+  })
   @IsString()
-  referral: string;
+  referredBy: string;
 
-  @IsDefined()
+  @IsDefined({
+    message: 'You must enter N.O.K details'
+  })
   @IsString()
   nokDetails: string;
 
@@ -161,8 +179,10 @@ export class CreatePatientDto {
   @IsArray()
   additionalContacts: string[];
 
+  @IsOptional()
   @IsDefined()
   @ValidateNested()
+  @Type(() => CreatePatientAssessmentDto)
   assessment: CreatePatientAssessmentDto;
 
 }
