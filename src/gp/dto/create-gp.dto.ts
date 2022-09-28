995a8f6a04bfa4337d1ced53ed27af2a1b00414d
Joseph Harris <joeharris461767@gmail.com>
2022-09-28 18:32:56 +0100

    Add validation message

--- src/gp/dto/create-gp.dto.ts
+++ src/gp/dto/create-gp.dto.ts
@@ -19,6 +19,9 @@ export class CreateGpDto {
     new RegExp(
       /^(((\+44\s?\d{4}|\(?0\d{4}\)?)\s?\d{3}\s?\d{3})|((\+44\s?\d{3}|\(?0\d{3}\)?)\s?\d{3}\s?\d{4})|((\+44\s?\d{2}|\(?0\d{2}\)?)\s?\d{4}\s?\d{4}))(\s?\#(\d{4}|\d{3}))?$/,
     ),
+    {
+      message: 'Please enter a valid telephone number'
+    }
   )
   phoneNumber: string;
 }
