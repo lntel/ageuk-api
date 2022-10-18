39ffbdcefb779d94fd7f0193389ef197490c30a9
Joseph Harris <joeharris461767@gmail.com>
2022-10-18 22:14:34 +0100

    Add tests

--- src/notifications/notifications.controller.spec.ts
+++ src/notifications/notifications.controller.spec.ts
@@ -5,10 +5,29 @@ import { NotificationsService } from './notifications.service';
 describe('NotificationsController', () => {
   let controller: NotificationsController;
 
+  const mockNotification = {
+    content: 'This is a test',
+    read: false
+  };
+
   beforeEach(async () => {
+
+    // https://stackoverflow.cóom/a/64720908
     const module: TestingModule = await Test.createTestingModule({
       controllers: [NotificationsController],
-      providers: [NotificationsService],
+      providers: [
+        {
+          provide: NotificationsService,
+          useValue: {
+            create: jest.fn(x => x),
+            findAll: jest.fn().mockResolvedValue([mockNotification]),
+            update: jest.fn(x => x).mockResolvedValue({
+              ...mockNotification,
+              read: true
+            }),
+          }
+        },
+      ],
     }).compile();
 
     controller = module.get<NotificationsController>(NotificationsController);
@@ -16,5 +35,39 @@ describe('NotificationsController', () => {
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  describe('create', () => {
+
+    const mockRecord = {
+      staff: 34,
+      content: 'This is a test'
+    };
+
+    it('should create a new notification', async () => {
+      const result = await controller.create(mockRecord);
+
+      expect(result).toStrictEqual(mockRecord);
+    });
+  });
+
+  describe('findAll', () => {
+
+    it('should find all notifications belonging to a staff member', async () => {
+      const result = await controller.findAll(34);
+
+      expect(result).toStrictEqual([mockNotification]);
+    });
+
+  });
+
+  describe('update', () => {
+
+    it('should mark the notification as read', async () => {
+      const result = await controller.update('1');
+
+      expect(result.read).toStrictEqual(true);
+    });
+
   });
 });
--- src/notifications/notifications.service.spec.ts
+++ src/notifications/notifications.service.spec.ts
@@ -1,12 +1,39 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { Staff } from '../staff/entities/staff.entity';
+import { StaffService } from '../staff/staff.service';
+import { Notification } from './entities/notification.entity';
 import { NotificationsService } from './notifications.service';
 
 describe('NotificationsService', () => {
   let service: NotificationsService;
 
   beforeEach(async () => {
+    const mockNotification = {
+      content: 'This is a test',
+    };
+
+    const mockStaff = {
+      forename: 'Test name',
+    };
+
     const module: TestingModule = await Test.createTestingModule({
-      providers: [NotificationsService],
+      providers: [
+        NotificationsService,
+        {
+          provide: getRepositoryToken(Notification),
+          useValue: {
+            save: jest.fn().mockResolvedValue(mockNotification),
+            find: jest.fn().mockResolvedValue([mockNotification]),
+          },
+        },
+        {
+          provide: StaffService,
+          useValue: {
+            findOne: jest.fn().mockResolvedValue(mockStaff),
+          },
+        },
+      ],
     }).compile();
 
     service = module.get<NotificationsService>(NotificationsService);
@@ -15,4 +42,5 @@ describe('NotificationsService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+
 });
