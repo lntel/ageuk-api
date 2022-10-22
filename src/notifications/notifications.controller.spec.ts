50e6b590110438b0714b63c3e20f00cd1b6492d0
Joseph Harris <joeharris461767@gmail.com>
2022-10-22 20:08:05 +0100

    Add tests

--- src/notifications/notifications.controller.spec.ts
+++ src/notifications/notifications.controller.spec.ts
@@ -1,4 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { PermissionGuard } from 'src/common/guards/permission.guard';
+import { StaffService } from 'src/staff/staff.service';
 import { NotificationsController } from './notifications.controller';
 import { NotificationsService } from './notifications.service';
 
@@ -8,8 +10,10 @@ describe('NotificationsController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [NotificationsController],
-      providers: [NotificationsService],
-    }).compile();
+      providers: [NotificationsService, StaffService],
+    })
+    .overrideGuard(PermissionGuard).useValue({ canActivate: jest.fn().mockReturnValue(true) })
+    .compile();
 
     controller = module.get<NotificationsController>(NotificationsController);
   });
