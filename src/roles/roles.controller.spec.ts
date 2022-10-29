5e263ecf22a4708fe3bfb2d08d177c140f5faec8
Joseph Harris <joeharris461767@gmail.com>
2022-10-29 12:47:08 +0100

    Add tests for roles controller

--- src/roles/roles.controller.spec.ts
+++ src/roles/roles.controller.spec.ts
@@ -1,52 +1,103 @@
-import { forwardRef } from "@nestjs/common";
-import { Test } from "@nestjs/testing";
-import { getRepositoryToken, TypeOrmModule } from "@nestjs/typeorm";
-import { Staff } from "src/staff/entities/staff.entity";
-import { Repository } from "typeorm";
-import { Role } from "./entities/role.entity";
-import { RolesController } from "./roles.controller"
-import { RolesService } from "./roles.service";
-import { PermissionTypeEnum } from "./types/Permissions";
+import { Test, TestingModule } from '@nestjs/testing';
+import { StaffService } from '../staff/staff.service';
+import { RolesController } from './roles.controller';
+import { RolesService } from './roles.service';
+import { PermissionTypeEnum } from './types/Permissions';
 
-// https://docs.nestjs.com/fundamentals/testing#unit-testing
-describe("RolesController", () => {
-    let rolesController: RolesController;
-    let rolesService: RolesService;
-    let rolesRepository: Repository<Role>;
+describe('RolesController', () => {
+  let controller: RolesController;
+  let service: RolesService;
 
-    beforeEach(async () => {
-        const moduleRef = await Test.createTestingModule({
-            controllers: [RolesController],
-            providers: [RolesService, {
-                provide: getRepositoryToken(Role),
-                useValue: {
-                    create: jest.fn(),
-                    save: jest.fn(),
-                    findOne: jest.fn(),
-                }
-            }, {
-                provide: getRepositoryToken(Staff),
-                useValue: {
-                    create: jest.fn(),
-                    save: jest.fn(),
-                    findOne: jest.fn(),
-                }
-            }]
-        }).compile();
+  const staffMock = {
+    sub: 1
+  }
 
-        rolesService = moduleRef.get<RolesService>(RolesService);
-        rolesController = moduleRef.get<RolesController>(RolesController);
-        rolesRepository = moduleRef.get<Repository<Role>>(getRepositoryToken(Role));
+  beforeEach(async () => {
+    // https://stackoverflow.cóom/a/64720908
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [RolesController],
+      providers: [
+        {
+          provide: StaffService,
+          useValue: {},
+        },
+        {
+          provide: RolesService,
+          useValue: {
+            create: jest.fn(),
+            findAll: jest.fn(),
+            findOne: jest.fn(),
+            update: jest.fn(),
+            remove: jest.fn(),
+          },
+        },
+      ],
+    }).compile();
+
+    controller = module.get<RolesController>(RolesController);
+    service = module.get<RolesService>(RolesService);
+  });
+
+  it('should be defined', () => {
+    expect(controller).toBeDefined();
+  });
+
+  describe('create', () => {
+
+    const mockDto = {
+        name: 'test role',
+        permissions: [PermissionTypeEnum.MANAGE_PATIENTS]
+    };
+
+    it('should call the create method on the service', async () => {
+        await controller.create(staffMock, mockDto);
+
+        expect(service.create).toHaveBeenCalledWith(staffMock, mockDto)
     });
+  });
+  
+  describe('findAll', () => {
+    it('should call the findAll method on the service', async () => {
+        await controller.findAll();
 
-    describe("findAll", () => {
-        it("should return an array of roles", async () => {
-            const result: Role[] = [];
-
-            jest.spyOn(rolesService, "findAll").mockImplementation(async () => result);
-
-            // expect(await rolesController.findAll()).toBe(result);
-            expect(rolesRepository).toBeDefined();
-        })
+        expect(service.findAll).toHaveBeenCalledTimes(1);
     });
+  });
+
+  describe('findOne', () => {
+
+    const id = '1';
+
+    it('should call the findOne method on the service', async () => {
+        await controller.findOne(id);
+
+        expect(service.findOne).toHaveBeenCalledWith(Number(id));
+    });
+  });
+
+  describe('update', () => {
+
+    const id = '1';
+    const mockDto = {
+        name: 'test role',
+        permissions: [PermissionTypeEnum.MANAGE_PATIENTS, PermissionTypeEnum.MANAGE_STAFF]
+    };
+
+    it('should call the update method on the service', async () => {
+        await controller.update(staffMock, id, mockDto);
+
+        expect(service.update).toHaveBeenCalledWith(staffMock, Number(id), mockDto);
+    });
+  });
+
+  describe('remove', () => {
+
+    const id = '1';
+
+    it('should call the remove method on the service', async () => {
+        await controller.remove(staffMock, id);
+
+        expect(service.remove).toHaveBeenCalledWith(staffMock, Number(id));
+    });
+  });
 });
