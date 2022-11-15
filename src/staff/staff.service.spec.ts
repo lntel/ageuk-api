b01cfa8f301654e365bba90c297458aaf717e592
Joseph Harris <joeharris461767@gmail.com>
2022-11-15 19:47:34 +0000

    Add more tests

--- src/staff/staff.service.spec.ts
+++ src/staff/staff.service.spec.ts
@@ -4,7 +4,7 @@ import { Repository } from 'typeorm';
 import { RolesService } from '../roles/roles.service';
 import { Staff } from './entities/staff.entity';
 import { StaffService } from './staff.service';
-import { compareSync, hashSync } from 'bcrypt';
+import * as bcrypt from 'bcrypt';
 import { HttpException } from '@nestjs/common';
 import { CreateStaffDto } from './dto/create-staff.dto';
 
@@ -20,18 +20,13 @@ describe('StaffController', () => {
         {
           provide: RolesService,
           useValue: {
-            findOne: jest.fn().mockReturnValue(undefined)
+            findOne: jest.fn().mockReturnValue(undefined),
           },
-        },
-        {
-          provide: Staff,
-          useValue: {
-            create: jest.fn()
-          }
         },
         {
           provide: getRepositoryToken(Staff),
           useValue: {
+            create: jest.fn((x) => x),
             save: jest.fn(),
             find: jest.fn(),
             findOne: jest.fn(),
@@ -110,6 +105,85 @@ describe('StaffController', () => {
   });
 
   describe('create', () => {
+    const mockStaff: CreateStaffDto = {
+      forename: 'john',
+      surname: 'doe',
+      emailAddress: 'johndoe@test.com',
+      dob: new Date(),
+      password: 'test',
+      roleId: 1,
+    };
+
+    it('should throw an exception if the email is already in use', async () => {
+      expect(async () => {
+        await service.create(mockStaff);
+      }).rejects.toThrow(HttpException);
+    });
+
+    it('should throw an exception if the role does not exist', async () => {
+      (entity.findOne as jest.Mock).mockReturnValueOnce(undefined);
+
+      expect(async () => {
+        await service.create(mockStaff);
+      }).rejects.toThrow(HttpException);
+    });
+
+    it('should call the hashSync method on bcrypt', async () => {
+      (rolesService.findOne as jest.Mock).mockReturnValue({
+        name: 'test role',
+      });
+
+      jest.spyOn(bcrypt, 'hashSync').mockReturnValueOnce('test');
+      (entity.findOne as jest.Mock).mockReturnValue(undefined);
+
+      await service.create(mockStaff);
+
+      expect(bcrypt.hashSync).toHaveBeenCalled();
+    });
+
+    it('should call the create method on the staff repository', async () => {
+      const roleMock = {
+        name: 'test role',
+      };
+
+      (rolesService.findOne as jest.Mock).mockReturnValueOnce(roleMock);
+
+      await service.create(mockStaff);
+
+      expect(entity.create).toHaveBeenCalledWith({
+        ...mockStaff,
+        role: roleMock,
+      });
+    });
+
+    it('should call the save method on the staff repository', async () => {
+      const mockRole = {
+        name: 'test role',
+      };
+
+      (rolesService.findOne as jest.Mock).mockReturnValue(mockRole);
+
+      await service.create(mockStaff);
+
+      expect(entity.save).toHaveBeenCalledWith({
+        ...mockStaff,
+        role: mockRole,
+      });
+    });
+  });
+
+  describe('findAll', () => {
+    it('should call the find method on the staff repository', async () => {
+      await service.findAll();
+
+      expect(entity.find).toHaveBeenCalledWith({
+        select: ['dob', 'emailAddress', 'forename', 'id', 'surname'],
+      });
+    });
+  });
+
+  describe('findOne', () => {
+    const staffId = 1;
 
     const mockStaff: CreateStaffDto = {
       forename: 'john',
@@ -117,32 +191,79 @@ describe('StaffController', () => {
       emailAddress: 'johndoe@test.com',
       dob: new Date(),
       password: 'test',
-      roleId: 1
-    }
+      roleId: 1,
+    };
 
-    it('should throw an exception if the email is already in use', async () => {
-      expect(async () => {
-        await service.create(mockStaff)
-      }).rejects.toThrow(HttpException);
+    it('should call the findOne method on the staff repository', async () => {
+      (entity.findOne as jest.Mock).mockReturnValueOnce(mockStaff);
+      
+      await service.findOne(staffId);
+      
+      expect(entity.findOne).toHaveBeenCalledWith({
+        where: {
+          id: staffId
+        },
+      });
     });
     
-    it('should throw an exception if the role does not exist', async () => {
+    it('should throw an exception if no staff found', async () => {
       (entity.findOne as jest.Mock).mockReturnValueOnce(undefined);
       
       expect(async () => {
-        await service.create(mockStaff)
+        await service.findOne(staffId);
+      }).rejects.toThrow(HttpException);
+      
+    });
+    
+    it('should return a staff object omitting the password field', async () => {
+      (entity.findOne as jest.Mock).mockReturnValueOnce(mockStaff);
+
+      const result = await service.findOne(staffId);
+
+      expect(result.password).toBeUndefined();
+    });
+  });
+
+  describe('findOneBy', () => {
+    const key = 'emailAddress';
+    const value = 'test@test.com';
+
+    const mockStaff: CreateStaffDto = {
+      forename: 'john',
+      surname: 'doe',
+      emailAddress: 'johndoe@test.com',
+      dob: new Date(),
+      password: 'test',
+      roleId: 1,
+    };
+
+    it('should call the findOne method on the staff repository', async () => {
+      (entity.findOne as jest.Mock).mockReturnValueOnce(mockStaff);
+      
+      await service.findOneBy(key, value);
+      
+      expect(entity.findOne).toHaveBeenCalledWith({
+        where: {
+          [key]: value,
+        },
+        relations: ['role']
+      });
+    });
+    
+    it('should throw an exception if no staff member is found', async () => {
+      (entity.findOne as jest.Mock).mockReturnValueOnce(undefined);
+      
+      expect(async () => {
+        await service.findOneBy(key, value);
       }).rejects.toThrow(HttpException);
     });
+    
+    it('should return the staff member', async () => {
+      (entity.findOne as jest.Mock).mockReturnValueOnce(mockStaff);
+      
+      const result = await service.findOneBy(key, value);
 
-    it('should call the hashSync method on bcrypt', async () => {
-      (rolesService.findOne as jest.Mock).mockReturnValue({
-        name: 'test role'
-      });
-      (entity.findOne as jest.Mock).mockReturnValue(undefined);
-
-      await service.create(mockStaff);
-
-      expect(hashSync).toHaveBeenCalled();
+      expect(result).toStrictEqual(mockStaff);
     });
   });
 });
