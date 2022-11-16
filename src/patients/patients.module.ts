416f65af004a514bba481a03ece8731bdb3dabcd
Joseph Harris <joeharris461767@gmail.com>
2022-11-16 21:46:09 +0000

    Add notification message

--- src/patients/patients.module.ts
+++ src/patients/patients.module.ts
@@ -1,15 +1,14 @@
 import { forwardRef, Module } from '@nestjs/common';
-import { PatientsService } from './patients.service';
-import { PatientsController } from './patients.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Patient } from './entities/patient.entity';
-import { GP } from '../gp/entities/gp.entity';
-import { GpService } from 'src/gp/gp.service';
 import { GpModule } from 'src/gp/gp.module';
+import { NotificationsModule } from 'src/notifications/notifications.module';
 import { Assessment } from './entities/assessment.entity';
+import { Patient } from './entities/patient.entity';
+import { PatientsController } from './patients.controller';
+import { PatientsService } from './patients.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Patient, Assessment]), forwardRef(() => GpModule)],
+  imports: [TypeOrmModule.forFeature([Patient, Assessment]), forwardRef(() => GpModule), NotificationsModule],
   controllers: [PatientsController],
   providers: [PatientsService],
   exports: [PatientsService]
