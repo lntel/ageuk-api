191bceb905261c6565df6debc762ad63be4b1911
Joseph Harris <joeharris461767@gmail.com>
2022-11-20 22:45:50 +0000

    Seperate interceptor

---
+++ src/staff/interceptors/file.interceptor.ts
@@ -0,0 +1,15 @@
+import { FileInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
+import { v4 } from 'uuid';
+
+// https://docs.nestjs.com/techniques/file-upload
+export const fileInterceptor = FileInterceptor('file', {
+  storage: diskStorage({
+    destination: './uploads',
+    filename: (req, file, cb) => {
+      const fileType = String(file.originalname).split('.')[1];
+      const filename = v4();
+      cb(null, `${filename}.${fileType}`);
+    },
+  }),
+});
