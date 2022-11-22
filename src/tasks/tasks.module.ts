49f40b524ae896a073750b54bb75c37fc41f040d
Joseph Harris <joeharris461767@gmail.com>
2022-11-22 21:49:26 +0000

    Add task to delete notifications

--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -1,7 +1,9 @@
 import { Module } from "@nestjs/common";
+import { NotificationsModule } from "src/notifications/notifications.module";
 import { TasksService } from "./tasks.service";
 
 @Module({
+    imports: [NotificationsModule],
     providers: [TasksService]
 })
 export class TasksModule {}
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,5 +1,6 @@
-import { Injectable, Logger } from "@nestjs/common";
+import { Inject, Injectable, Logger } from "@nestjs/common";
 import { Cron } from "@nestjs/schedule";
+import { NotificationsService } from "src/notifications/notifications.service";
 
 // * https://docs.nestjs.com/techniques/task-scheduling#example
 
@@ -8,9 +9,38 @@ export class TasksService {
 
     private readonly logger = new Logger(TasksService.name);
 
+    constructor(
+        @Inject(NotificationsService)
+        private readonly notificationService: NotificationsService
+    ) {}
+
     @Cron('0 0 * * *')
     handleNetworkTraining() {
         this.logger.log('Neural Network training maintenance')
     }
 
+    @Cron('0 2 * * *')
+    async handleNotificationDeletion() {
+        this.logger.log(`[${Date.now()}] Checking for outdated notifications`);
+
+        const notifications = await this.notificationService.findAll();
+
+        const readNotifications = notifications.filter(notification => notification.read);
+        const unreadNotifications = notifications.filter(notification => !notification.read);
+
+        const threeDaysAgo = new Date(Date.now() - 72 * 60 * 60 * 1000);
+        const oneWeekAgo  = new Date(Date.now() - 120 * 60 * 60 * 1000);
+
+        const outdated = [
+            ...readNotifications.filter(notification => notification.lastUpdated <= threeDaysAgo),
+            ...unreadNotifications.filter(notification => notification.lastUpdated <= oneWeekAgo),
+        ];
+
+        if(!outdated.length)
+            return this.logger.log(`[${Date.now()}] No outdated notifications found`);
+
+        outdated.forEach(async notification => await notification.remove());
+
+        this.logger.log(`[${Date.now()}] ${outdated.length} notifications deleted`);
+    }
 }
